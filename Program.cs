31e69dd21822778bbe62f72c94fb1cb4d7c86fba
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-03-08 11:25:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Csharp_Returning_Value
+{
+    class Program
+    {
+        //Return Type Method
+            //This function is calculating area
+        static double returningtest(int r){
+            double area = r * r * 3.14;
+            return area;
+
+        }
+
+        static void Main(string[] args)
+        {
+            //The value of radius is initilized
+            int radius=5;
+            //Function call
+            double result = returningtest(radius);
+            Console.WriteLine("Area is  {0}", result);
+
+
+
+        }
+    }
+}
